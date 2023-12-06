2fea235f173276db2e5c95979235f3b4978f92f4
yoonseongho <sxdz1290@naver.com>
2023-12-06 16:56:40 +0900

    Method

---
+++ ch01/Method.js
@@ -0,0 +1,10 @@
+/** Property
+ * 데이터 타입마다 가지고 있는 고유한 속성들
+ */
+
+/** Array.length
+ * 특정 배열의 길이를 나타내는 속성
+ * 길이 => 배열 내 요소의 개수
+ */
+let ranking = ["Jason", "Alice", "Chris", "Jane", "Tom"];
+console.log(ranking.length);
