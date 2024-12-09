6225e44ce6cb5155728d2a9845d064dbe51578cd
SeeQ <sxdz1290@naver.com>
2024-12-09 23:19:25 +0900

    todoList - end

--- ch03/Todo-list/script.js
+++ ch03/Todo-list/script.js
@@ -1,20 +1,34 @@
 const todoInput = document.querySelector('#todo-input');
 const todoList = document.querySelector('#todo-list');
 
-const createTodo = () => {
+const savedTodoList = JSON.parse(localStorage.getItem('save-items'));
+
+const createTodo = (storageData) => {
+    let todoContents = todoInput.value;
+
+    if (storageData) {
+        todoContents = storageData.contents;
+    }
+
     const newLi = document.createElement('li');
     const newSpan = document.createElement('span');
     const newBtn = document.createElement('button');
 
     newBtn.addEventListener('click', () => {
         newLi.classList.toggle('complete');
+        saveItemsFn();
     });
 
     newLi.addEventListener('dblclick', () => {
         newLi.remove();
+        saveItemsFn();
     });
 
-    newSpan.textContent = todoInput.value;
+    if (storageData?.complete) {
+        newLi.classList.add('complete');
+    }
+
+    newSpan.textContent = todoContents;
     newLi.appendChild(newBtn);
     newLi.appendChild(newSpan);
     todoList.appendChild(newLi);
@@ -34,16 +48,26 @@ const deleteAll = () => {
     for (let i = 0; i < liList.length; i++) {
         liList[i].remove();
     }
-}
+
+    saveItemsFn();
+};
 
 const saveItemsFn = () => {
     const saveItems = [];
-    for (let i = 0; i < todoList.children.length; i ++) {
+    for (let i = 0; i < todoList.children.length; i++) {
         const todoObj = {
             contents: todoList.children[i].querySelector('span').textContent,
             complete: todoList.children[i].classList.contains('complete')
-        }
+        };
         saveItems.push(todoObj);
     }
-    console.log(saveItems);
+
+    saveItems.length === 0 ? localStorage.removeItem('save-items') : localStorage.setItem('save-items', JSON.stringify(saveItems));
+
+};
+
+if (savedTodoList) {
+    for (let i = 0; i < savedTodoList.length; i++) {
+        createTodo(savedTodoList[i]);
+    }
 }
